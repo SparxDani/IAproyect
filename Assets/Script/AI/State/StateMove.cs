7726a2fc4fc1917802edbb3805d6b630fcf9c38c
SparxDani <86084707+SparxDani@users.noreply.github.com>
2024-09-09 07:19:47 -0500

    Wall Avoidance

--- Assets/Script/AI/State/StateMove.cs
+++ Assets/Script/AI/State/StateMove.cs
@@ -6,17 +6,22 @@ public class StateMove : StateBase
 {
     protected SteeringBehavior _SteeringBehavior;
     public Transform place;
-    // Start is called before the first frame update
+
     public override void LoadComponent()
     {
         _SteeringBehavior = GetComponent<SteeringBehavior>();
         base.LoadComponent();
     }
+
     public virtual void MoveToPlace()
     {
         Vector3 steeringForce = _SteeringBehavior.Arrive(place);
 
-        _SteeringBehavior.ClampMagnitude(steeringForce);
+        Vector3 avoidForce = _SteeringBehavior.WallAvoidance();
+
+        Vector3 totalForce = steeringForce + avoidForce;
+
+        _SteeringBehavior.ClampMagnitude(totalForce);
 
         _SteeringBehavior.UpdatePosition();
     }
